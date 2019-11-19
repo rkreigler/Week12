452805cd555a64e3e1777c9806314ccdd3595df7
Ryan Kreigler <rkreigler@mines.edu>
2019-11-19 10:10:54 -0700

    added load file function

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,18 @@
 def PrintOutput(s):
     print("OUTPUT", s)
+
+def LoadFile(filename):
+    file = open(filename)
+    contents = file.read()
+    list = []
+    t = 0
+    for i in range(len(contents)):
+        if contents[i] == "\n":
+            list.append(contents[t:i])
+            t = i
+        elif i == len(contents) - 1:
+            list.append(contents[t:i+1])
+    for i in range(1,len(list)):
+        list[i] = list[i][1:]
+    return list
+        
